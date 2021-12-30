c9bec664f13136666a8c7696c8e7e26eeb7f5c97
rafikee <rafik.vcu@gmail.com>
2021-12-30 12:19:22 -0800

    added pychromecast test

--- bilal_backend/views.py
+++ bilal_backend/views.py
@@ -1,5 +1,21 @@
 from bilal_backend import app
+import pychromecast
+from flask import request
 
 @app.route('/')
 def hello_world():
     return "Hello World"
+
+
+@app.route('/test-sound', methods ['POST', 'GET'])
+def test_sound(speaker_name = 'Studio Display', audio_id = '1jishJEjKVBqMqLhR4uPv8X8hjOKIIvgS'):
+    data = request.get_json()
+    speaker_name = data['speaker_name']
+    audio_id = data['audio_id']
+    chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[speaker_name])
+    cast = chromecasts[0]
+    cast.wait()
+    cast.set_volume(.5)
+    mc = cast.media_controller
+    mc.play_media(f'https://drive.google.com/uc?export=download&id={audio_id}', 'audio/mp3')
+    return "Sound is played"
