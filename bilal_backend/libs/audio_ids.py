1baf17ddca53f4afc0b26084eb371c780f370fdd
rafikee <rafik.vcu>
2022-01-01 16:23:20 -0800

    added athan dict

---
+++ bilal_backend/libs/audio_ids.py
@@ -0,0 +1,33 @@
+'''
+audio = {
+    "audio file ID in Google URL" : {
+        "name" : "Name of file",
+        "length" : "string that describes the length of the file",
+        "id" : "audio file ID in Google URL",
+        "type" : fajr or takbir or notifications or athans or dua
+    }
+}
+'''
+
+audio = {
+    "1yqji2O4arMpd5-PZUpvNyKx-Xfj9KTOv" : {
+        "name" : "Nasir Al-Qatami",
+        "length" : "10s",
+        "id" : "1yqji2O4arMpd5-PZUpvNyKx-Xfj9KTOv",
+        "type" : "takbir"
+    },
+
+    "1aSmUrA6JdqrCBLnNiLspwQsWj5oW_vo7" : {
+        "name" : "Mohammad Al-Amin Mosque Beirut",
+        "length" : "2m37s",
+        "id" : "1aSmUrA6JdqrCBLnNiLspwQsWj5oW_vo7",
+        "type" : "fajr"
+    },
+
+    "1JsS_zHJgmJAqSyEhHLOjQhKZPEoYajZN" : {
+        "name" : "Mohamed Tarek",
+        "length" : "2m30s",
+        "id" : "1JsS_zHJgmJAqSyEhHLOjQhKZPEoYajZN",
+        "type" : "athan"
+    },
+}
--- bilal_backend/routes/client_speakers.py
+++ bilal_backend/routes/client_speakers.py
@@ -15,6 +15,8 @@ class Speakers(MethodView):
     def get(self):
         return get_speakers()
 
+# TODO add get speaker method and return 412 if cannot connect to speaker in DB otherwise return speaker
+
 
 @speakers.route('/play')
 class PlaySound(MethodView):
@@ -22,3 +24,6 @@ class PlaySound(MethodView):
     @output(PlayedSchema)
     def post(self, data):
         return play_sound(data['audio_id'])
+
+
+# TODO add test sound, takes audio_id and speaker object
