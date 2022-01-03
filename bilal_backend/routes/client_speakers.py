27947d1dd7d09c00919a6e8535cb469485cc968a
rafikee <rafik.vcu>
2022-01-02 23:55:02 -0800

    added TRIES to constant

--- bilal_backend/routes/client_speakers.py
+++ bilal_backend/routes/client_speakers.py
@@ -1,6 +1,6 @@
 from apiflask import APIBlueprint, output, input, abort, doc
 from flask.views import MethodView
-from bilal_backend.libs.chromecast_handler import get_speakers, play_sound, test_sound, get_speaker
+from bilal_backend.libs.chromecast_handler import get_speakers, play_sound, test_sound
 from bilal_backend.scrap import get_chromecast
 from bilal_backend.spec.schemas import SpeakersSchema, PlayedSchema, PlaySchema, TestSoundSchema, SpeakerSchema
 
@@ -9,24 +9,12 @@ speakers = APIBlueprint(import_name="Speaker",
                         tag="Speakers",
                         url_prefix='/speakers')
 
-
 @speakers.route('/')
 class Speakers(MethodView):
     @output(SpeakersSchema)
     def get(self):
         return get_speakers()
 
-''' Work in Progress
-@speakers.route('/speaker')
-class Speaker(MethodView):
-    @output(SpeakerSchema)
-    @doc(responses=[200, 412])
-    def get(self):
-        resp = get_chromecast()
-        if not resp:
-            abort(status_code=412, message="Unable to connect to speaker")
-        return resp
-'''
 
 @speakers.route('/play')
 class PlaySound(MethodView):
