7f19ab453f6b994985286c59bf5d33a53b0cdeef
rafikee <rafik.vcu>
2022-01-04 12:18:13 -0800

    added schedule_athans

--- bilal_backend/libs/pt_handler.py
+++ bilal_backend/libs/pt_handler.py
@@ -3,6 +3,6 @@ from datetime import date
 from bilal_backend.utils.prayer_times import PrayTimes
 
 
-def prayer_times_handler(lat=None, long=None, tz=None, calc=None):
+def prayer_times_handler(lat=None, long=None, tz=None, calc=None, format=None):
     pray_times = PrayTimes(calMethod=calc)
-    return pray_times.getTimes(date=date.today(), coords=(lat, long), timezone=tz)
+    return pray_times.getTimes(date=date.today(), coords=(lat, long), timezone=tz, format=format)
