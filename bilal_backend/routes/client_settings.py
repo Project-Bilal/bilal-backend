ef75497da10faef5dbe4be5757b8474f2f4deba5
Omar Sarhan <omars@zillowgroup.com>
2022-01-05 17:13:43 -0800

    rm wifi endpoint

--- bilal_backend/routes/client_settings.py
+++ bilal_backend/routes/client_settings.py
@@ -78,19 +78,6 @@ class Volume(MethodView):
         return 'success'
 
 
-@settings.get('/wifi')
-@doc(responses=[200])
-def get_wifi():
-    if subprocess.getoutput("uname -m") == 'x86_64':
-        # is mac
-        return subprocess.getoutput(
-            "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk -F:  '($1 ~ \"^ *SSID$\"){print $2}' | cut -c 2-"
-        )
-    else:
-        # is rpi
-        return subprocess.getoutput("iwgetid -r")
-
-
 @settings.delete('/reset')
 @doc(responses=[200])
 def reset():
