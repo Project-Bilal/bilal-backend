e8694c43005660ad4a448012160274e6e3844806
Omar Sarhan <omars@zillowgroup.com>
2022-01-06 22:51:08 -0800

    fixed how athans were being saved in json db

--- bilal_backend/libs/constants.py
+++ bilal_backend/libs/constants.py
@@ -3,3 +3,12 @@ GDRIVE_URL = "https://drive.google.com/uc?export=download&id="
 DISCOVER_TIMEOUT = 5
 THUMB = "15SQIn_doa-SUdo65Zkp4ayfruMvCNwBC"
 DEFAULT_AUDIO_TITLE = "Project-Bilal"
+SUCCESS = 'success'
+
+
+class PrayerNames:
+    FAJR = 'fajr'
+    DHUHR = 'dhuhr'
+    ASR = 'asr'
+    MAGHRIB = 'maghrib'
+    ISHA = 'isha'
--- bilal_backend/routes/client_athans.py
+++ bilal_backend/routes/client_athans.py
@@ -3,6 +3,7 @@ from flask.views import MethodView
 from bilal_backend.utils.audio_ids import audio
 from bilal_backend.libs import athans_handler as handlers
 from bilal_backend.spec.schemas import AudioSchema
+from bilal_backend.libs.constants import PrayerNames, SUCCESS
 
 athans = APIBlueprint(import_name="Athans", name="Athans", tag="Athan", url_prefix='/athans')
 
@@ -12,88 +13,89 @@ def get_athans():
     return audio
 
 
-@athans.route('/fajir')
-class FajirAthan(MethodView):
+@athans.route(f'/{PrayerNames.FAJR}')
+class FajrAthan(MethodView):
     def get(self):
-        resp = handlers.get_fajir_athan()
+        resp = handlers.get_athan(PrayerNames.FAJR)
         if not resp:
-            abort(status_code=412, message="No fajir audio id saved")
+            abort(status_code=412, message="No fajr audio id saved")
         return resp
 
     @input(AudioSchema)
     def put(self, audio_id):
-        handlers.set_fajir_athan(audio_id)
-        return 'success'
+        handlers.set_athan(PrayerNames.FAJR, audio_id)
+        return SUCCESS
 
 
-@athans.route('/dhuhr')
+@athans.route(f'/{PrayerNames.DHUHR}')
 class DhuhrAthan(MethodView):
     def get(self):
-        resp = handlers.get_dhuhr_athan()
+        resp = handlers.get_athan(PrayerNames.DHUHR)
         if not resp:
             abort(status_code=412, message="No dhuhr audio id saved")
         return resp
 
     @input(AudioSchema)
     def put(self, audio_id):
-        handlers.set_dhuhr_athan(audio_id)
-        return 'success'
+        handlers.set_athan(PrayerNames.DHUHR, audio_id)
+        return SUCCESS
 
 
-@athans.route('/asr')
+@athans.route(f'/{PrayerNames.ASR}')
 class AsrAthan(MethodView):
     def get(self):
-        resp = handlers.get_asr_athan()
+        resp = handlers.get_athan(PrayerNames.ASR)
         if not resp:
             abort(status_code=412, message="No asr audio id saved")
         return resp
 
     @input(AudioSchema)
     def put(self, audio_id):
-        handlers.set_asr_athan(audio_id)
-        return 'success'
+        handlers.set_athan(PrayerNames.ASR, audio_id)
+        return SUCCESS
 
 
-@athans.route('/mughrib')
-class MughribAthan(MethodView):
+@athans.route(f'/{PrayerNames.MAGHRIB}')
+class MaghribAthan(MethodView):
     def get(self):
-        resp = handlers.get_mughrib_athan()
+        resp = handlers.get_athan(PrayerNames.MAGHRIB)
         if not resp:
-            abort(status_code=412, message="No mughrib audio id saved")
+            abort(status_code=412, message="No maghrib audio id saved")
         return resp
 
     @input(AudioSchema)
     def put(self, audio_id):
-        handlers.set_mughrib_athan(audio_id)
-        return 'success'
+        handlers.set_athan(PrayerNames.MAGHRIB, audio_id)
+        return SUCCESS
 
 
-@athans.route('/isha')
+@athans.route(f'/{PrayerNames.ISHA}')
 class IshaAthan(MethodView):
     def get(self):
-        resp = handlers.get_isha_athan()
+        resp = handlers.get_athan(PrayerNames.ISHA)
         if not resp:
             abort(status_code=412, message="No isha audio id saved")
         return resp
 
     @input(AudioSchema)
     def put(self, audio_id):
-        handlers.set_isha_athan(audio_id)
-        return 'success'
+        handlers.set_athan(PrayerNames.ISHA, audio_id)
+        return SUCCESS
 
 
 @athans.route('/notification')
 class NotificationAthan(MethodView):
     def get(self):
-        resp = handlers.get_notification()
+        resp = handlers.get_athan('notification')
         if not resp:
             abort(status_code=412, message="No notication audio id saved")
         return resp
 
     @input(AudioSchema)
     def put(self, audio_id):
-        handlers.set_notification(audio_id)
-        return 'success'
+        handlers.set_athan('notification', audio_id)
+        return SUCCESS
+
 
 @athans.route('/schedule')
 class ScheduleAthan(MethodView):
