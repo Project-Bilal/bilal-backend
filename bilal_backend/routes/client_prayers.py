9840ee9b4fcde62d19e2d3b652c7d12fbb093f63
rafikee <rafik.vcu@gmail.com>
2022-01-09 00:49:02 -0800

    updated get for calc method

--- bilal_backend/routes/client_prayers.py
+++ bilal_backend/routes/client_prayers.py
@@ -6,28 +6,41 @@ from bilal_backend.libs.pt_handler import prayer_times_handler
 from bilal_backend.spec.schemas import PrayerTimesSchemas
 from bilal_backend.utils.utils import calculations
 
-prayer_times = APIBlueprint(import_name="Prayer Times",
-                            name="Prayer Times",
-                            tag="Prayer Times",
-                            url_prefix='/prayer-times')
+prayer_times = APIBlueprint(
+    import_name="Prayer Times",
+    name="Prayer Times",
+    tag="Prayer Times",
+    url_prefix="/prayer-times",
+)
 
 
-@prayer_times.get('/')
+@prayer_times.get("/")
 @output(PrayerTimesSchemas)
 @doc(responses=[200, 412])
 @db_context
 def get_prayer_times(data):
-    calc = data.get('calculation')
-    location = data.get('location')
-    if not calc or not location or 'lat' not in location or 'long' not in location or 'tz' not in location:
-        abort(status_code=412, message=f"No user settings found. calc = {calc}, location = {location}")
-    return prayer_times_handler(lat=location.get('lat'),
-                                long=location.get('long'),
-                                tz=location.get('tz'),
-                                calc=calc)
+    calc = data.get("calculation", {}).get("method", {})
+    location = data.get("location")
+    if (
+        not calc
+        or not location
+        or "lat" not in location
+        or "long" not in location
+        or "tz" not in location
+    ):
+        abort(
+            status_code=412,
+            message=f"No user settings found. calc = {calc}, location = {location}",
+        )
+    return prayer_times_handler(
+        lat=location.get("lat"),
+        long=location.get("long"),
+        tz=location.get("tz"),
+        calc=calc,
+    )
 
 
-@prayer_times.get('/calculations')
+@prayer_times.get("/calculations")
 @doc(responses=[200])
 def get_calculations():
     return calculations
