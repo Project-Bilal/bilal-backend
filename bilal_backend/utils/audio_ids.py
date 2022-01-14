69bf3f14669571412573869edad666e71e52b943
rafikee <rafik.vcu@gmail.com>
2022-01-14 01:51:45 -0800

    added athans

--- bilal_backend/utils/audio_ids.py
+++ bilal_backend/utils/audio_ids.py
@@ -10,12 +10,6 @@ audio = {
 """
 
 audio = {
-    "1yqji2O4arMpd5-PZUpvNyKx-Xfj9KTOv": {
-        "name": "Nasir Al-Qatami",
-        "length": "10s",
-        "audio_id": "1yqji2O4arMpd5-PZUpvNyKx-Xfj9KTOv",
-        "type": "takbir",
-    },
     "1aSmUrA6JdqrCBLnNiLspwQsWj5oW_vo7": {
         "name": "Mohammad Al-Amin Mosque Beirut",
         "length": "2m37s",
@@ -27,6 +21,42 @@ audio = {
         "length": "2m30s",
         "audio_id": "1JsS_zHJgmJAqSyEhHLOjQhKZPEoYajZN",
         "type": "athan",
+    },
+    "1xcm_FH9g98XIfi20v_ZEkVGlogWPPAAb": {
+        "name": "Mishary Al Afasy Kuwait",
+        "length": "2m35s",
+        "audio_id": "1xcm_FH9g98XIfi20v_ZEkVGlogWPPAAb",
+        "type": "athan",
+    },
+    "1D1GfkpBflMRGWK7iCoA6fcd8MGDXsQ3f": {
+        "name": "Nasser Al Qattami KSA",
+        "length": "2m8s",
+        "audio_id": "1D1GfkpBflMRGWK7iCoA6fcd8MGDXsQ3f",
+        "type": "athan",
+    },
+    "1PhEwqjrcsGBf_ka8mAT5W_KJxhABFKIq": {
+        "name": "Ameen Ka3sees Algeria",
+        "length": "3m4s",
+        "audio_id": "1PhEwqjrcsGBf_ka8mAT5W_KJxhABFKIq",
+        "type": "athan",
+    },
+    "1mGtIirLjg9w9FqqaF_ym5rYK5AR_3mOX": {
+        "name": "Mohammed Al Amin Oman",
+        "length": "3m24s",
+        "audio_id": "1mGtIirLjg9w9FqqaF_ym5rYK5AR_3mOX",
+        "type": "athan",
+    },
+    "1OYnvJw6C65VhoeA2lI0f2DqfVTMPa9Rm": {
+        "name": "Bosnia",
+        "length": "2m49s",
+        "audio_id": "1OYnvJw6C65VhoeA2lI0f2DqfVTMPa9Rm",
+        "type": "athan",
+    },
+    "1yqji2O4arMpd5-PZUpvNyKx-Xfj9KTOv": {
+        "name": "Nasir Al-Qatami KSA",
+        "length": "10s",
+        "audio_id": "1yqji2O4arMpd5-PZUpvNyKx-Xfj9KTOv",
+        "type": "takbir",
     },
     "1jishJEjKVBqMqLhR4uPv8X8hjOKIIvgS": {
         "name": "tweet",
