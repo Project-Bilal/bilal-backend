76f08104a94c2ff49a03aac71d0181722c1bae78
rafikee <rafik.vcu@gmail.com>
2022-01-28 00:35:47 -0800

    reverted to old audio_id

--- bilal_backend/utils/audio_ids.py
+++ bilal_backend/utils/audio_ids.py
@@ -1,65 +1,76 @@
-{
+"""
+audio = {
+    "audio file ID in Google URL" : {
+        "name" : "Name of file",
+        "length" : "string that describes the length of the file",
+        "id" : "audio file ID in Google URL",
+        "type" : fajr or takbir or notifications or athans or dua
+    }
+}
+"""
+
+audio = {
+    "athan_fajr_1": {
+        "name": "Athan Fajr 1",
+        "desc": "Mohammad Al-Amin Mosque Beirut",
+        "length": "2m37s",
+        "audio_id": "athan_fajr_1",
+        "type": "fajr",
+    },
     "athan_1": {
-        "audio_id": "athan_1",
+        "name": "Athan 1",
         "desc": "Mishary Al Afasy Kuwait",
         "length": "2m34s",
-        "name": "Athan 1",
+        "audio_id": "athan_1",
         "type": "athan",
     },
     "athan_2": {
-        "audio_id": "athan_2",
+        "name": "Athan 2",
         "desc": "Mohamed Tarek",
         "length": "2m30s",
-        "name": "Athan 2",
-        "type": "athan",
-    },
-    "athan_3": {
-        "audio_id": "athan_3",
-        "desc": "Ameen Ka3sees Algeria",
-        "length": "3m4s",
-        "name": "Athan 3",
+        "audio_id": "athan_2",
         "type": "athan",
     },
     "athan_4": {
-        "audio_id": "athan_4",
+        "name": "Athan 4",
         "desc": "Nasser Al Qattami KSA",
         "length": "2m7s",
-        "name": "Athan 4",
+        "audio_id": "athan_4",
         "type": "athan",
     },
-    "athan_5": {
-        "audio_id": "athan_5",
-        "desc": "Bosnia",
-        "length": "2m49s",
-        "name": "Athan 5",
+    "athan_3": {
+        "name": "Athan 3",
+        "desc": "Ameen Ka3sees Algeria",
+        "length": "3m4s",
+        "audio_id": "athan_3",
         "type": "athan",
     },
     "athan_6": {
-        "audio_id": "athan_6",
+        "name": "Athan 6",
         "desc": "Mohammed Al Amin Oman",
         "length": "3m24s",
-        "name": "Athan 6",
+        "audio_id": "athan_6",
         "type": "athan",
     },
-    "athan_fajr_1": {
-        "audio_id": "athan_fajr_1",
-        "desc": "Mohammad Al-Amin Mosque Beirut",
-        "length": "2m37s",
-        "name": "Athan Fajr 1",
-        "type": "fajr",
+    "athan_5": {
+        "name": "Athan 5",
+        "desc": "Bosnia",
+        "length": "2m49s",
+        "audio_id": "athan_5",
+        "type": "athan",
     },
     "takbir_1": {
-        "audio_id": "takbir_1",
+        "name": "Takbir 1",
         "desc": "Nasir Al-Qatami KSA",
         "length": "10s",
-        "name": "Takbir 1",
+        "audio_id": "takbir_1",
         "type": "takbir",
     },
     "tweet": {
-        "audio_id": "tweet",
+        "name": "Tweet",
         "desc": "Tweet",
         "length": "1s",
-        "name": "Tweet",
+        "audio_id": "tweet",
         "type": "notification",
     },
 }
